7fe212d983eaa6bd92289e850e282e8e33e837fb
klaa97 <10521251@polimi.it>
2021-08-29 00:58:34 +0200

    chore: modularize cube rendering

--- js/index.js
+++ js/index.js
@@ -20,6 +20,7 @@ import { SpecularSideBar } from "./ui/sidebar/specular.js";
 import { SpecularRenderer } from "./render/specular.js";
 import { ToggleSideBar } from "./ui/sidebar/toggle.js";
 import { CubeSideBar } from "./ui/sidebar/cube.js";
+import { CubeRenderer } from "./render/cube.js";
 
 // Check and retrieve webgl rendering context
 const canvas = document.getElementById("canvas");
@@ -90,195 +91,38 @@ let ambientRenderer = new AmbientRenderer(
 let diffuseRenderer = new DiffuseRenderer(diffuseState, gl, program);
 let specularRenderer = new SpecularRenderer(specularState, gl, program);
 await ambientRenderer.loadTexture(gl);
+let cubeRenderer = new CubeRenderer(cube, gl, program, textureDir);
+await cubeRenderer.loadTexture();
 
 // Clear viewport
 gl.clearColor(0.85, 1.0, 0.85, 1.0);
 gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 gl.enable(gl.DEPTH_TEST);
 
-// Obtain locations for common uniforms
-const positionAttributeLocation = gl.getAttribLocation(program, "a_position");
-const normalAttributeLocation = gl.getAttribLocation(program, "a_normal");
-const worldViewProjectionMatrixLocation = gl.getUniformLocation(
-  program,
-  "worldViewProjectionMatrix"
-);
-const worldViewMatrixLocation = gl.getUniformLocation(
-  program,
-  "worldViewMatrix"
-);
-const transposeViewMatrixLocation = gl.getUniformLocation(
-  program,
-  "transposeViewMatrix"
-);
-const normalMatrixLocation = gl.getUniformLocation(program, "normalMatrix");
-
-const uvAttributeLocation = gl.getAttribLocation(program, "a_textureCoord");
-const tangentAttributeLocation = gl.getAttribLocation(program, "a_tangent");
-const bitangentAttributeLocation = gl.getAttribLocation(program, "a_bitangent");
-const textLocation = gl.getUniformLocation(program, "u_texture");
-const normalMapLocation = gl.getUniformLocation(program, "u_normalMap");
-
-for (let i = 0; i < 26; i++) {
-  let vao = gl.createVertexArray();
-  cube.pieceArray[i].vao = vao;
-  gl.bindVertexArray(vao);
-
-  let vertices = cube.pieceArray[i].vertices;
-  let normals = cube.pieceArray[i].normals;
-  let indices = cube.pieceArray[i].indices;
-  let uvCoords = cube.pieceArray[i].textures;
-  let tangents = cube.pieceArray[i].tangents;
-  let bitangents = cube.pieceArray[i].bitangents;
-
-  let positionBuffer = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
-  gl.enableVertexAttribArray(positionAttributeLocation);
-  gl.vertexAttribPointer(positionAttributeLocation, 3, gl.FLOAT, false, 0, 0);
-
-  const normalsBuffer = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, normalsBuffer);
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(normals), gl.STATIC_DRAW);
-  gl.enableVertexAttribArray(normalAttributeLocation);
-  gl.vertexAttribPointer(normalAttributeLocation, 3, gl.FLOAT, false, 0, 0);
-
-  const indexBuffer = gl.createBuffer();
-  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
-  gl.bufferData(
-    gl.ELEMENT_ARRAY_BUFFER,
-    new Uint16Array(indices),
-    gl.STATIC_DRAW
-  );
-
-  const uvBuffer = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, uvBuffer);
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(uvCoords), gl.STATIC_DRAW);
-  gl.enableVertexAttribArray(uvAttributeLocation);
-  gl.vertexAttribPointer(uvAttributeLocation, 2, gl.FLOAT, false, 0, 0);
-
-  const tangentBuffer = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, tangentBuffer);
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(tangents), gl.STATIC_DRAW);
-  gl.enableVertexAttribArray(tangentAttributeLocation);
-  gl.vertexAttribPointer(tangentAttributeLocation, 3, gl.FLOAT, false, 0, 0);
-
-  const bitangentBuffer = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, bitangentBuffer);
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(bitangents), gl.STATIC_DRAW);
-  gl.enableVertexAttribArray(bitangentAttributeLocation);
-  gl.vertexAttribPointer(bitangentAttributeLocation, 3, gl.FLOAT, false, 0, 0);
-}
-
-// Set the image sources
-const image = new Image();
-image.src = `${textureDir}customCubeTexture.png`;
-const normalImage = new Image();
-normalImage.src = `${textureDir}customCubeNormal.png`;
-
-// Create texture
-const texture = gl.createTexture();
-// Load the texture
-await image.decode();
-gl.activeTexture(gl.TEXTURE0);
-gl.bindTexture(gl.TEXTURE_2D, texture);
-gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
-gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
-gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
-gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-gl.generateMipmap(gl.TEXTURE_2D);
-
-// Create normal texture
-const normalTexture = gl.createTexture();
-// Load the normal texture
-await normalImage.decode();
-gl.activeTexture(gl.TEXTURE0 + 1);
-gl.bindTexture(gl.TEXTURE_2D, normalTexture);
-gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
-gl.texImage2D(
-  gl.TEXTURE_2D,
-  0,
-  gl.RGBA,
-  gl.RGBA,
-  gl.UNSIGNED_BYTE,
-  normalImage
-);
-gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
-gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-gl.generateMipmap(gl.TEXTURE_2D);
-
 function drawFrame() {
   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
   gl.useProgram(program);
 
+  // Setup main matrices
   matrices.perspectiveMatrix = canvasState.perspectiveMatrix;
-
   matrices.viewMatrix = cameraState.viewMatrix;
+  matrices.transposeViewMatrix = mathUtils.transposeMatrix(
+    cameraState.viewMatrix
+  );
 
   // Matrix to transform light direction from world to camera space
   let lightDirMatrix = mathUtils.invertMatrix(
     mathUtils.transposeMatrix(matrices.viewMatrix)
   );
 
-  // Matrix to convert normals from camera space to world space
-  let transposeViewMatrix = mathUtils.transposeMatrix(matrices.viewMatrix);
-  gl.uniform1i(textLocation, 0);
-  gl.uniform1i(normalMapLocation, 1);
-  gl.uniformMatrix4fv(
-    transposeViewMatrixLocation,
-    false,
-    mathUtils.transposeMatrix(transposeViewMatrix)
-  );
-
+  // Inject uniforms
   lightRenderer.injectUniform(matrices.viewMatrix, lightDirMatrix);
   ambientRenderer.injectUniform(matrices.viewMatrix, lightDirMatrix);
   diffuseRenderer.injectUniform();
   specularRenderer.injectUniform();
 
-  // Draw cubes
-  for (let i = 0; i < 26; i++) {
-    let worldMatrix = cube.pieceArray[i].worldMatrix;
-
-    let viewWorldMatrix = mathUtils.multiplyMatrices(
-      matrices.viewMatrix,
-      worldMatrix
-    );
-
-    // Inverse transpose of the world view matrix for the normals
-    let normalMatrix = mathUtils.invertMatrix(
-      mathUtils.transposeMatrix(viewWorldMatrix)
-    );
-
-    let projectionMatrix = mathUtils.multiplyMatrices(
-      matrices.perspectiveMatrix,
-      viewWorldMatrix
-    );
-
-    gl.uniformMatrix4fv(
-      worldViewProjectionMatrixLocation,
-      false,
-      mathUtils.transposeMatrix(projectionMatrix)
-    );
-    gl.uniformMatrix4fv(
-      worldViewMatrixLocation,
-      false,
-      mathUtils.transposeMatrix(viewWorldMatrix)
-    );
-    gl.uniformMatrix4fv(
-      normalMatrixLocation,
-      false,
-      mathUtils.transposeMatrix(normalMatrix)
-    );
-
-    // Bind VAO to obtain buffers set outside of the rendering loop
-    gl.bindVertexArray(cube.pieceArray[i].vao);
-    gl.drawElements(
-      gl.TRIANGLES,
-      cube.pieceArray[i].indices.length,
-      gl.UNSIGNED_SHORT,
-      0
-    );
-  }
+  // Draw the cubes
+  cubeRenderer.renderCube(matrices);
 
   // Draw skybox if the ambient is skybox
   if (ambientState.type === "skybox")
---
+++ js/render/cube.js
@@ -0,0 +1,220 @@
+import { mathUtils } from "../utils.js";
+
+/**
+ * Manage the rendering of the Rubik cube :cube:
+ * @param {CubeState} cube
+ * @param {WebGL2RenderingContext} gl
+ * @param {WebGLProgram} program
+ * @param {string} textureDir
+ */
+export function CubeRenderer(cube, gl, program, textureDir) {
+  this.init = function () {
+    // Obtain locations for common uniforms
+    const positionAttributeLocation = gl.getAttribLocation(
+      program,
+      "a_position"
+    );
+    const normalAttributeLocation = gl.getAttribLocation(program, "a_normal");
+    this.worldViewProjectionMatrixLocation = gl.getUniformLocation(
+      program,
+      "worldViewProjectionMatrix"
+    );
+    this.worldViewMatrixLocation = gl.getUniformLocation(
+      program,
+      "worldViewMatrix"
+    );
+    this.transposeViewMatrixLocation = gl.getUniformLocation(
+      program,
+      "transposeViewMatrix"
+    );
+
+    const uvAttributeLocation = gl.getAttribLocation(program, "a_textureCoord");
+    const tangentAttributeLocation = gl.getAttribLocation(program, "a_tangent");
+    const bitangentAttributeLocation = gl.getAttribLocation(
+      program,
+      "a_bitangent"
+    );
+    this.textLocation = gl.getUniformLocation(program, "u_texture");
+    this.normalMapLocation = gl.getUniformLocation(program, "u_normalMap");
+
+    for (let i = 0; i < 26; i++) {
+      let vao = gl.createVertexArray();
+      cube.pieceArray[i].vao = vao;
+      gl.bindVertexArray(vao);
+
+      let vertices = cube.pieceArray[i].vertices;
+      let normals = cube.pieceArray[i].normals;
+      let indices = cube.pieceArray[i].indices;
+      let uvCoords = cube.pieceArray[i].textures;
+      let tangents = cube.pieceArray[i].tangents;
+      let bitangents = cube.pieceArray[i].bitangents;
+
+      let positionBuffer = gl.createBuffer();
+      gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+      gl.bufferData(
+        gl.ARRAY_BUFFER,
+        new Float32Array(vertices),
+        gl.STATIC_DRAW
+      );
+      gl.enableVertexAttribArray(positionAttributeLocation);
+      gl.vertexAttribPointer(
+        positionAttributeLocation,
+        3,
+        gl.FLOAT,
+        false,
+        0,
+        0
+      );
+
+      const normalsBuffer = gl.createBuffer();
+      gl.bindBuffer(gl.ARRAY_BUFFER, normalsBuffer);
+      gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(normals), gl.STATIC_DRAW);
+      gl.enableVertexAttribArray(normalAttributeLocation);
+      gl.vertexAttribPointer(normalAttributeLocation, 3, gl.FLOAT, false, 0, 0);
+
+      const indexBuffer = gl.createBuffer();
+      gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+      gl.bufferData(
+        gl.ELEMENT_ARRAY_BUFFER,
+        new Uint16Array(indices),
+        gl.STATIC_DRAW
+      );
+
+      const uvBuffer = gl.createBuffer();
+      gl.bindBuffer(gl.ARRAY_BUFFER, uvBuffer);
+      gl.bufferData(
+        gl.ARRAY_BUFFER,
+        new Float32Array(uvCoords),
+        gl.STATIC_DRAW
+      );
+      gl.enableVertexAttribArray(uvAttributeLocation);
+      gl.vertexAttribPointer(uvAttributeLocation, 2, gl.FLOAT, false, 0, 0);
+
+      const tangentBuffer = gl.createBuffer();
+      gl.bindBuffer(gl.ARRAY_BUFFER, tangentBuffer);
+      gl.bufferData(
+        gl.ARRAY_BUFFER,
+        new Float32Array(tangents),
+        gl.STATIC_DRAW
+      );
+      gl.enableVertexAttribArray(tangentAttributeLocation);
+      gl.vertexAttribPointer(
+        tangentAttributeLocation,
+        3,
+        gl.FLOAT,
+        false,
+        0,
+        0
+      );
+
+      const bitangentBuffer = gl.createBuffer();
+      gl.bindBuffer(gl.ARRAY_BUFFER, bitangentBuffer);
+      gl.bufferData(
+        gl.ARRAY_BUFFER,
+        new Float32Array(bitangents),
+        gl.STATIC_DRAW
+      );
+      gl.enableVertexAttribArray(bitangentAttributeLocation);
+      gl.vertexAttribPointer(
+        bitangentAttributeLocation,
+        3,
+        gl.FLOAT,
+        false,
+        0,
+        0
+      );
+    }
+  };
+
+  /**
+   * Load the texture
+   */
+  this.loadTexture = async function () {
+    // Set the image sources to start the loading
+    const image = new Image();
+    image.src = `${textureDir}customCubeTexture.png`;
+    const normalImage = new Image();
+    normalImage.src = `${textureDir}customCubeNormal.png`;
+
+    // Create cube texture
+    const texture = gl.createTexture();
+    // Load the texture
+    await image.decode();
+    gl.activeTexture(gl.TEXTURE0);
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+    gl.generateMipmap(gl.TEXTURE_2D);
+
+    // Create normal texture
+    const normalTexture = gl.createTexture();
+    // Load the normal texture
+    await normalImage.decode();
+    gl.activeTexture(gl.TEXTURE0 + 1);
+    gl.bindTexture(gl.TEXTURE_2D, normalTexture);
+    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+    gl.texImage2D(
+      gl.TEXTURE_2D,
+      0,
+      gl.RGBA,
+      gl.RGBA,
+      gl.UNSIGNED_BYTE,
+      normalImage
+    );
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+    gl.generateMipmap(gl.TEXTURE_2D);
+  };
+
+  this.init();
+
+  /**
+   * Render the rubik cube bby drawing the 26 cubes
+   * @param {{perspectiveMatrix: number[], viewMatrix: number[], transposeViewMatrix: number[]}} matrices
+   */
+  this.renderCube = function (matrices) {
+    gl.uniform1i(this.textLocation, 0);
+    gl.uniform1i(this.normalMapLocation, 1);
+    gl.uniformMatrix4fv(
+      this.transposeViewMatrixLocation,
+      false,
+      mathUtils.transposeMatrix(matrices.transposeViewMatrix)
+    );
+    // Draw cubes
+    for (let i = 0; i < 26; i++) {
+      let worldMatrix = cube.pieceArray[i].worldMatrix;
+
+      let viewWorldMatrix = mathUtils.multiplyMatrices(
+        matrices.viewMatrix,
+        worldMatrix
+      );
+
+      let projectionMatrix = mathUtils.multiplyMatrices(
+        matrices.perspectiveMatrix,
+        viewWorldMatrix
+      );
+
+      gl.uniformMatrix4fv(
+        this.worldViewProjectionMatrixLocation,
+        false,
+        mathUtils.transposeMatrix(projectionMatrix)
+      );
+      gl.uniformMatrix4fv(
+        this.worldViewMatrixLocation,
+        false,
+        mathUtils.transposeMatrix(viewWorldMatrix)
+      );
+
+      // Bind VAO to obtain buffers set outside of the rendering loop
+      gl.bindVertexArray(cube.pieceArray[i].vao);
+      gl.drawElements(
+        gl.TRIANGLES,
+        cube.pieceArray[i].indices.length,
+        gl.UNSIGNED_SHORT,
+        0
+      );
+    }
+  };
+}
